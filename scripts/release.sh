a119e51f51f83c4ba8289582703b9eef7394da84
Jes Struck <mail@jesstruck.dk>
2024-11-19 10:14:28 +0100

    ci: Print CURRENT and NEXT version tag
    Making the release script print both the CURRENT and the NEXT release tag so we have a better understanding of which version bump is done.

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,16 +2,17 @@
 
 set -e
 
-export TAG=$(svu next)
+export CURRENT_TAG=$(svu)
+export NEXT_TAG=$(svu next)
 
-read -p "Creating new release for $TAG. Do you want to continue? [Y/n] " prompt
+read -p "Creating new release for $NEXT_TAG (Last version: $CURRENT_TAG). Do you want to continue? [Y/n] " prompt
 
 if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]; then
 	python3 scripts/prepare_changelog.py
 	git add CHANGELOG.md
-	git commit -m "chore: bump version to $TAG for release" || true && git push
-	echo "Creating new git tag $TAG"
-	git tag "$TAG" -m "$TAG"
+	git commit -m "chore: bump version to $NEXT_TAG for release" || true && git push
+	echo "Creating new git tag $NEXT_TAG"
+	git tag "$NEXT_TAG" -m "$NEXT_TAG"
 	git push --tags
 else
 	echo "Cancelled"
